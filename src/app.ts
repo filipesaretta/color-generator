62de07540839b134d56acce5569becbf0c192f94
filipesaretta <filipessm124@gmail.com>
2022-11-28 21:02:01 -0300

    Add the possibility to copy to the clipboard

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,29 @@
-const generatorButton = document.querySelector<HTMLElement>('button')
-const bg = document.querySelector<HTMLElement>('main')
+const generatorButton = document.querySelector<HTMLElement>('button');
+const bg = document.querySelector<HTMLElement>('main');
+const colorHex = document.querySelector('.hex');
 
 function generateColors() {
-  const color = '#' + Math.floor(Math.random() * (0xffffff + 1)).toString(16).padStart(6, 0);
-  const hexText = bg?.querySelector('.hex');
+  const color = '#' + Math.floor(Math.random() * (0xffffff + 1)).toString(16).padStart(6, '0');
 
-  if (bg && hexText) {
+  if (bg && colorHex) {
     const button = bg.querySelector('button')
     button?.style.borderColor
     bg.style.backgroundColor = color;
-    hexText.textContent = color
+    colorHex.textContent = color
   }
 
 }
 
 
+
+function copyToClipBoard() {
+  // TODO: Need a look to add an alert as it was copied
+  navigator.clipboard.writeText(colorHex?.textContent!)
+
+}
+
+
 generatorButton?.addEventListener('click', generateColors);
+colorHex?.addEventListener('click', copyToClipBoard);
 
 generateColors()
